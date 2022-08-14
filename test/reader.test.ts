4cc97b649e17890240edc259e89cc9467c504bee
Yongwook Choi <flow@hrmm.xyz>
2022-08-14 17:56:24 +0900

    Add more testcases for reader

--- test/reader.test.ts
+++ test/reader.test.ts
@@ -1,10 +1,44 @@
 import { open } from '../reader.ts';
-import { assertEquals } from 'https://deno.land/std@0.152.0/testing/asserts.ts';
+import {
+  assertEquals,
+  assertRejects,
+} from 'https://deno.land/std@0.152.0/testing/asserts.ts';
 
-Deno.test('Read one from test1.cdb', async () => {
+Deno.test('Read from test1.cdb', async ctx => {
   const cdb = await open('./test/test1.cdb');
-  const result = await cdb.getFirst('one');
-  const text = new TextDecoder().decode(result);
-  assertEquals(text, 'Hello');
+
+  await ctx.step('Get values of `one` using `Get`', async () => {
+    const results = ['Hello', ', World!'];
+    for await (const result of cdb.get('one')) {
+      const text = new TextDecoder().decode(result);
+      assertEquals(text, results.shift());
+    }
+  });
+
+  await ctx.step('Get first value of `one`', async () => {
+    const result = await cdb.getFirst('one');
+    const text = new TextDecoder().decode(result);
+    assertEquals(text, 'Hello');
+  });
+
+  await ctx.step('Get first value of long key', async () => {
+    const result = await cdb.getFirst(
+      'this key will be split across two reads'
+    );
+    const text = new TextDecoder().decode(result);
+    assertEquals(text, 'Got it.');
+  });
+
+  await ctx.step('Get second value of `one`', async () => {
+    const result = await cdb.getByOffset('one', 1);
+    const text = new TextDecoder().decode(result);
+    assertEquals(text, ', World!');
+  });
+
+  await ctx.step('Throw an error when there is no value', async () => {
+    await assertRejects(async () => await cdb.getFirst('merong'));
+    await assertRejects(async () => await cdb.getByOffset('two', 1));
+  });
+
   cdb.close();
 });
